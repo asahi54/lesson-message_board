e1975df4f3f5f8da09c26eb8c0dff1858bbf4b40
asahi.shi <symtk54@gmail.com>
2019-07-15 03:02:09 +0000

    add column

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -59,7 +59,7 @@ class MessagesController < ApplicationController
     @message = Message.find(params[:id])
   
   def message_params
-    params.require(:message).permit(:content)
+    params.require(:message).permit(:content, :title)
   end
   
 end
